d27b75d019e50e81d322e98f6b4edacbdb6a1de1
Dragons Gamers <DragonsGamers@hotmail.com.br>
2020-04-21 22:18:27 -0300

    test(): Rename public folder for dist

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,11 @@ module.exports = {
   output: {
     filename: 'ODGMask.js',
     publicPath: '',
-    path: path.resolve(__dirname, 'public/'),
+    path: path.resolve(__dirname, 'dist/'),
   },
   devServer: {
     host: "0.0.0.0",
     port: 7075,
-    contentBase: path.join(__dirname, 'public')
+    contentBase: path.join(__dirname, 'dist')
   }
 }
